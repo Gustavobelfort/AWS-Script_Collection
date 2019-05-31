aca3ba45567359d23401bd7c172bca8bae9b4837
Gustavo Belfort <gustavombelfort@gmail.com>
2019-05-31 01:09:11 -0300

    save_all_users reading from the env var

--- Cognito/save_all_users_to_csv.sh
+++ Cognito/save_all_users_to_csv.sh
@@ -2,6 +2,6 @@
 
 USER_POOL_ID = $POOL_ID
 
-aws cognito-idp list-users --user-pool-id ${USER_POOL_ID} |  jq --raw-output '.Users[] | {user: .Username, email: .Attributes[] | select(.Name == email).Value} | map(.) | @csv' >> users.csv
+aws cognito-idp list-users --user-pool-id  ${USER_POOL_ID} |  jq --raw-output '.Users[] | {user: .Username, email: .Attributes[] | select(.Name == "email").Value} | map(.) | @csv' >> users.csv
 
 echo "Done."
